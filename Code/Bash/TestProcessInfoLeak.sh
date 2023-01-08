524279a4d7280a3ecf4f13611baea4a052e395e3
Graham Gold <graham@cirriustech.co.uk>
2023-01-08 15:44:40 +0000

    Minor tweak
    Amend comments in code

--- Code/Bash/TestProcessInfoLeak.sh
+++ Code/Bash/TestProcessInfoLeak.sh
@@ -7,5 +7,5 @@ cat /proc/$$/environ
 ps -ww -fp $$
 # If you have HISTCONTROL set to ignoreboth or ignorespace and start bash commands at prompt with a space they don’t go in bash history
 # . /path/to/script.sh args1text = in bash history, but nothing in /proc/<pid>/cmdline or in ps output
-# prefix with a space, not in bash history, still not in ps or cmdline
+# prefix with a space, not in bash history, still in ps & cmdline
 # bash -c "run stuff" = all bets are off
